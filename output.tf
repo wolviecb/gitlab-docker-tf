702774fe472cb0c968f6941804d37143348a1da6
Tom Andrade <wolvie@gmail.com>
2019-09-29 17:28:57 +0200

    remove dns outputs as they are being moved to a dedicated dns module

--- output.tf
+++ output.tf
@@ -1,15 +1,3 @@
 output "ip" {
   value = google_compute_instance.git.network_interface[0].access_config[0].nat_ip
 }
-
-output "git" {
-  value = google_dns_record_set.git_dns.name
-}
-
-output "short" {
-  value = google_dns_record_set.short_dns.name
-}
-
-output "root" {
-  value = google_dns_record_set.root_dns.name
-}
